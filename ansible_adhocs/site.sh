e2321686271d99fb2662d8b1d89009f2d2ed4df0
mirschao <mirschao@gmail.com>
2024-10-31 00:07:30 +0800

    initial

---
+++ ansible_adhocs/site.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# author: mirschao
+# date: 2024/10/30
+# usage: ansible adhoc execute site interface.
+
+
+function scan_kernel_version() {
+    ansible -i inventory/hosts_machines machines -m shell -a 'uname -r'
+}
+
+function main() {
+    scan_kernel_version || return $?
+}
+
+main "$@"
